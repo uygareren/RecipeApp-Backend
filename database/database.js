2a3a8f8630c9e602737763694adf6497a5d497b9
Uygar Eren <komifb90@hotmail.com>
2024-05-11 00:25:13 +0300

    envs were updated

--- database/database.js
+++ database/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 const MongoClient = mongodb.MongoClient;
+require('dotenv').config();
 
-export const _db = mongoose.connect('mongodb+srv://recipeapp:123@recipeapp.neok85z.mongodb.net/', {useNewUrlParser:true, useUnifiedTopology:true})
+export const _db = mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
